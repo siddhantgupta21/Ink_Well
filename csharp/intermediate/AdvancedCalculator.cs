bcb70c8afc778364491dad992579635613aa8872
siddhant gupta <siddhant.gupta21b@iiitg.ac.in>
2025-01-22 18:22:41 +0530

    Added csharp intermediate assingment

---
+++ csharp/intermediate/AdvancedCalculator.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace CIntermediate
+{
+    public class AdvancedCalculator : Calculator
+    {
+        public int Power(int baseNum, int exponent)
+        {
+            int result = (int)Math.Pow(baseNum, exponent);
+            SetResult(result);
+            return result;
+        }
+
+        public override double GetResult()
+        {
+            return base.GetResult() * 1_000_000; 
+        }
+    }
+}
