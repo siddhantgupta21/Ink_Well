64d326997ce8b2ac824bf07e0a242683d935c45f
siddhant gupta <siddhant.gupta21b@iiitg.ac.in>
2025-01-22 22:12:22 +0530

    Add intermediate assingment

--- csharp/intermediate/ICalculator.cs
+++ csharp/intermediate/ICalculator.cs
@@ -1,10 +1,10 @@
-namespace CIntermediate
-{
-    public interface ICalculator
-    {
-        int Add(int a, int b);
-        int Add(int a, int b, int c);
-        float Add(float a, float b);
-        double GetResult(); 
-    }
-}
+// namespace CIntermediate
+// {
+//     public interface ICalculator
+//     {
+//         int Add(int a, int b);
+//         int Add(int a, int b, int c);
+//         float Add(float a, float b);
+//         double GetResult(); 
+//     }
+// }
