0b301a7c8ba14649d8c41b5f4446e7a97ec7211b
siddhant gupta <siddhant.gupta21b@iiitg.ac.in>
2025-01-22 22:14:49 +0530

    Added assingment csharpintermediate

--- csharp/intermediate/Calculator.cs
+++ csharp/intermediate/Calculator.cs
@@ -1,35 +1,35 @@
-// using System;
+using System;
 
-// namespace CIntermediate
-// {
-//     public class Calculator : ICalculator
-//     {
-//         private double Result { get; set; }
-//         protected void SetResult(double value)
-//         {
-//             Result = value;
-//         }
+namespace CIntermediate
+{
+    public class Calculator : ICalculator
+    {
+        private double Result { get; set; }
+        protected void SetResult(double value)
+        {
+            Result = value;
+        }
 
-//         public int Add(int a, int b)
-//         {
-//             Result = a + b;
-//             return (int)Result;
-//         }
+        public int Add(int a, int b)
+        {
+            Result = a + b;
+            return (int)Result;
+        }
 
-//         public int Add(int a, int b, int c)
-//         {
-//             Result = a + b + c;
-//             return (int)Result;
-//         }
+        public int Add(int a, int b, int c)
+        {
+            Result = a + b + c;
+            return (int)Result;
+        }
 
-//         public float Add(float a, float b)
-//         {
-//             Result = a + b;
-//             return (float)Result;
-//         }
-//         public virtual double GetResult()
-//         {
-//             return Result;
-//         }
-//     }
-// }
+        public float Add(float a, float b)
+        {
+            Result = a + b;
+            return (float)Result;
+        }
+        public virtual double GetResult()
+        {
+            return Result;
+        }
+    }
+}
--- csharp/intermediate/Program.cs
+++ csharp/intermediate/Program.cs
@@ -1,20 +1,20 @@
-﻿// using System;
+﻿using System;
 
-// namespace CIntermediate
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
+namespace CIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-//             Calculator calc = new Calculator();
-//             Console.WriteLine(calc.Add(2,3));
-//             Console.WriteLine(calc.Add(4,5,6));
-//             Console.WriteLine(calc.Add(2.5f,1.5f));
-//             Console.WriteLine(calc.GetResult());
-//             var adv=new AdvancedCalculator();
-//             Console.WriteLine(adv.Power(3,3));
-//             Console.WriteLine(adv.GetResult());
-//         }
-//     }
-// }
+            Calculator calc = new Calculator();
+            Console.WriteLine(calc.Add(2,3));
+            Console.WriteLine(calc.Add(4,5,6));
+            Console.WriteLine(calc.Add(2.5f,1.5f));
+            Console.WriteLine(calc.GetResult());
+            var adv=new AdvancedCalculator();
+            Console.WriteLine(adv.Power(3,3));
+            Console.WriteLine(adv.GetResult());
+        }
+    }
+}
